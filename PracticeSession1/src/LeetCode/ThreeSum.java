caa5c20656c5d1bfcc71f1e90124daa6b6844fc7
MisVandi <vandi.misra@gmail.com>
2024-05-08 02:27:38 +0530

    Practice Session

--- PracticeSession1/src/LeetCode/ThreeSum.java
+++ PracticeSession1/src/LeetCode/ThreeSum.java
@@ -1,4 +1,61 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+
+//https://leetcode.com/problems/3sum/description/
 public class ThreeSum {
+    public static void main(String[] args) {
+        int[] a = {0,0,0};
+        //int target = 19;
+        List<List<Integer>> ans = new ArrayList<>();
+        HashMap<Integer, Integer> newTable = new HashMap<>();
+        System.out.println(threeSum(a));
+    }
+
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        HashMap<Integer,Integer> nhash = new HashMap<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i=0;i<nums.length;i++){
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int left=i+1;
+            int right=nums.length-1;
+
+            while(left<right){
+                int total = nums[i]+nums[left]+nums[right];
+                if(total>0){
+                    right--;
+                }
+                else if(total<0){
+                    left++;
+                }
+                else{
+                    ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left++;
+                    while(nums[left]==nums[left-1]&&left<right)
+                        left++;
+                }
+            }
+        }
+        /*
+        for(int i=1;i<nums.length-1;i++){
+            int target = nums[i-1]==0?0:-(nums[i-1]);
+            if(nhash.containsKey(target-nums[i])){
+                ans.add(Arrays.asList(nums[i-1],nums[i],target-nums[i]));
+            }
+            nhash.put(nums[i-1],i-1);
+            nhash.put(nums[i],i);
+
+        }
+        */
+        return ans;
+    }
 }
--- PracticeSession1/src/LeetCode/TwoSum.java
+++ PracticeSession1/src/LeetCode/TwoSum.java
@@ -1,4 +1,59 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+//https://leetcode.com/problems/two-sum/
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 public class TwoSum {
+    public static void main(String[] args) {
+        int[] a = {16, 4, 23, 8, 15, 42, 1, 2};
+        int target = 19;
+        HashMap<Integer,Integer> newTable =  new HashMap<>();
+
+        for (int i=0;i<a.length;i++){
+            if(newTable.containsKey(target-a[i])){
+                System.out.println(newTable.get(target-a[i])+","+i);
+            }
+            newTable.put(a[i],i);
+        }
+
+
+
+        //int[] a2 = a.clone();
+
+
+        //O(n)=Onlogn
+        /*
+        //int[] a = {2,15,11,7};
+        ArrayList<Integer> al = new ArrayList<>();
+        int target = 19;
+        Arrays.sort(a);
+      //  int[] a = Arrays.stream(aa).sorted().toArray();
+        int left = 0;
+        int right =a.length-1;
+        while(left<right){
+            if(a[left]+a[right] > target){
+                right--;
+            }
+            else if(a[left]+a[right] < target){
+                left++;
+            }
+            else if(a[left]+a[right] == target){
+                al.add(Arrays.asList(a2).indexOf(a[left]));
+                al.add(Arrays.asList(a2).indexOf(a[right]));
+               // al.add(right);
+                System.out.println(al);
+                break;
+            }
+            else{
+                System.out.println("Not found");
+                break;
+            }
+        }
+    }
+
+         */
+    }
 }
